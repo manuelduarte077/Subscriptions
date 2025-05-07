0ec4714af40b0c9581525896d2a29ad0d7ee3c32
manuelduarte077 <carlosd99m@gmail.com>
2025-05-06 23:09:26 -0600

    refactor: update to Material3 components and simplify UI

--- composeApp/src/wasmJsMain/kotlin/dev/donmanuel/monthlybill/app/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package dev.donmanuel.monthlybill.app
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
