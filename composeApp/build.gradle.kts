e7e7732501b8f1a0b4453e53de7599d2fa779c65
manuelduarte077 <carlosd99m@gmail.com>
2025-05-08 02:50:17 -0600

    refactor: update build configuration by removing unused dependencies and adjusting SDK versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,17 +1,12 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-
-    id("app.cash.sqldelight") version "2.0.2"
-    kotlin("plugin.serialization") version "2.0.21"
 }
 
 kotlin {
@@ -35,34 +30,12 @@ kotlin {
 
     jvm("desktop")
 
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        moduleName = "composeApp"
-        browser {
-            val rootDirPath = project.rootDir.path
-            val projectDirPath = project.projectDir.path
-            commonWebpackConfig {
-                outputFileName = "composeApp.js"
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
-                }
-            }
-        }
-        binaries.executable()
-    }
-
     sourceSets {
         val desktopMain by getting
 
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
-
-            implementation(libs.android.driver)
         }
 
         commonMain.dependencies {
@@ -75,15 +48,8 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
 
-            implementation(libs.koin.core)
-            implementation(libs.runtime)
-
             //Navigation
             implementation(libs.navigation.compose)
-        }
-
-        iosMain.dependencies {
-            implementation(libs.native.driver)
         }
 
         desktopMain.dependencies {
@@ -132,15 +98,6 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "dev.donmanuel.monthlybill.app"
             packageVersion = "1.0.0"
-        }
-    }
-}
-
-sqldelight {
-    databases {
-        create("MonthlyBillDatabase") {
-            packageName.set("dev.donmanuel.monthlybill.app")
-            generateAsync.set(true)
         }
     }
 }
