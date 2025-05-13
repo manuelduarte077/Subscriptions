46951c216882b3b0161c113753c51ec9b2547583
manuelduarte077 <carlosd99m@gmail.com>
2025-05-13 00:53:38 -0600

    feat: improve UI layout

--- composeApp/src/commonMain/kotlin/dev/donmanuel/monthlybill/app/features/categories/data/repositories/CategoryRepositoryImp.kt
+++ composeApp/src/commonMain/kotlin/dev/donmanuel/monthlybill/app/features/categories/data/repositories/CategoryRepositoryImp.kt
@@ -5,12 +5,16 @@ import dev.donmanuel.monthlybill.app.features.categories.domain.repositories.Cat
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import monthlybill.composeapp.generated.resources.Res
-import monthlybill.composeapp.generated.resources.apple
 import monthlybill.composeapp.generated.resources.disneyplus
+import monthlybill.composeapp.generated.resources.duolingo
 import monthlybill.composeapp.generated.resources.hbo
 import monthlybill.composeapp.generated.resources.netflix
 import monthlybill.composeapp.generated.resources.primevideo
 import monthlybill.composeapp.generated.resources.spotify
+import monthlybill.composeapp.generated.resources.twitch
+import monthlybill.composeapp.generated.resources.x
+import monthlybill.composeapp.generated.resources.youtube
+import monthlybill.composeapp.generated.resources.zoom
 
 class CategoryRepositoryImp : CategoryRepository {
     override fun getSubscriptions(): Flow<List<Category>?> {
@@ -29,32 +33,50 @@ class CategoryRepositoryImp : CategoryRepository {
         ),
         Category(
             id = 2,
-            name = "Spotify",
-            icon = Res.drawable.spotify,
-            color = ""
-        ),
-        Category(
-            id = 3,
             name = "Disney+",
             icon = Res.drawable.disneyplus,
             color = ""
         ),
         Category(
-            id = 4,
+            id = 3,
             name = "HBO",
             icon = Res.drawable.hbo,
             color = ""
         ),
         Category(
-            id = 5,
+            id = 4,
             name = "Amazon Prime",
             icon = Res.drawable.primevideo,
             color = ""
         ),
         Category(
+            id = 5,
+            name = "Duolingo",
+            icon = Res.drawable.duolingo,
+            color = ""
+        ),
+        Category(
             id = 6,
-            name = "Apple TV+",
-            icon = Res.drawable.apple,
+            name = "Youtube",
+            icon = Res.drawable.youtube,
+            color = ""
+        ),
+        Category(
+            id = 7,
+            name = "X",
+            icon = Res.drawable.x,
+            color = ""
+        ),
+        Category(
+            id = 8,
+            name = "Twitch",
+            icon = Res.drawable.twitch,
+            color = ""
+        ),
+        Category(
+            id = 9,
+            name = "Spotify",
+            icon = Res.drawable.spotify,
             color = ""
         ),
     )
--- composeApp/src/commonMain/kotlin/dev/donmanuel/monthlybill/app/features/categories/presentation/ui/CategoryScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/donmanuel/monthlybill/app/features/categories/presentation/ui/CategoryScreen.kt
@@ -1,62 +1,103 @@
 package dev.donmanuel.monthlybill.app.features.categories.presentation.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.donmanuel.monthlybill.app.features.categories.domain.entities.Category
 import dev.donmanuel.monthlybill.app.features.categories.presentation.viewmodel.CategoriesViewModel
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoryScreen(
     modifier: Modifier = Modifier,
 ) {
-
     val viewModel = koinViewModel<CategoriesViewModel>()
     val categories by viewModel.getAllCategories()
         .collectAsStateWithLifecycle(initialValue = emptyList())
 
-    Scaffold { innerPadding ->
-        Box(contentAlignment = Alignment.Center) {
-            LazyColumn(modifier = modifier.padding(innerPadding)) {
-                itemsIndexed(items = categories.orEmpty()) { index, item ->
-                    CategoryItem(categories = item)
-                }
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Categories",
+                        style = MaterialTheme.typography.titleMedium,
+                        fontSize = 20.sp,
+                    )
+                },
+            )
+        },
+    ) { innerPadding ->
+        LazyVerticalGrid(
+            modifier = modifier.padding(innerPadding),
+            columns = GridCells.Fixed(2),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            itemsIndexed(items = categories.orEmpty()) { index, item ->
+                CategoryItem(
+                    category = item
+                )
             }
         }
     }
 }
 
 @Composable
-fun CategoryItem(categories: Category) {
+fun CategoryItem(category: Category) {
 
-    Row(
+    Card(
         modifier = Modifier
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .fillMaxWidth()
+            .padding(8.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondary),
     ) {
-        Icon(
-            painter = painterResource(categories.icon),
-            contentDescription = categories.name,
-            tint = MaterialTheme.colorScheme.primary,
-            modifier = Modifier.size(24.dp)
-        )
-        Spacer(modifier = Modifier.padding(8.dp))
-        Text(text = categories.name)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                modifier = Modifier
+                    .width(64.dp)
+                    .height(64.dp),
+                painter = painterResource(category.icon),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = category.name,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onPrimary,
+            )
+        }
     }
 }
